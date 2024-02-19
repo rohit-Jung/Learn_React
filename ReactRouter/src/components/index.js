ff16a96b53e0cd952bf4ac34a949c0f8daf25b77
rohit-Jung <rokshh28@gmail.com>
2024-02-19 22:14:06 +0545

    Learning React Router

---
+++ ReactRouter/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Home from "./Home/Home";
+
+export {Header, Footer, About, Contact, Home}
