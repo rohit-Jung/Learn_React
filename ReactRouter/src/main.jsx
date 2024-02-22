dbbf01dcd8d200a6e0e58e336ce5bd1c3760a74f
rohit-Jung <rokshh28@gmail.com>
2024-02-22 09:58:19 +0545

    creating router the other way

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -11,7 +11,7 @@ import Layout from "./Layout";
 import { About, Contact, Github, Home, Users } from "./components";
 import { gitInfo } from "./components/Github/Github";
 
-//creating router paths -> createBrowserRouter -> createRoutesFromElements 
+//creating router paths -> createBrowserRouter -> createRoutesFromElements
 const router = createBrowserRouter(
   createRoutesFromElements(
     //Route can be self closing or pair closing based on our needs to loop like here all the routes are defined inside the main route that contains layout information
@@ -21,14 +21,46 @@ const router = createBrowserRouter(
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/:params" element={<Users />} />
-      <Route 
-      loader={gitInfo} // We used the gitInfo variable from the Component Github to load the information fetched in that variable
-      path="/github" 
-      element={<Github />} 
+      <Route
+        loader={gitInfo} // We used the gitInfo variable from the Component Github to load the information fetched in that variable
+        path="/github"
+        element={<Github />}
       />
     </Route>
   )
-)
+);
+
+//Router Paths can be created in this way too
+//createBrowserRouter([{}])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: ":params",
+//         element: <Users />,
+//       },
+//       {
+//         path: "github",
+//         element: <Github />,
+//         loader: gitInfo,
+//       },
+//     ],
+//   },
+// ]);
 
 //providing the router and routes -> RouterProvider router ={router}
 ReactDOM.createRoot(document.getElementById("root")).render(
