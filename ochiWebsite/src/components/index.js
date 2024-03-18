a76c3b9269bc803d4c5fc570b64d9843fc165189
rohit-Jung <rokshh28@gmail.com>
2024-03-18 21:35:50 +0545

    updated index of components

---
+++ ochiWebsite/src/components/index.js
@@ -0,0 +1,10 @@
+import AboutPage from "./AboutPage";
+import LandingPage from "./LandingPage";
+import Featured from "./Featured";
+import Eyes from "./Eyes";
+import Navbar from "./Navbar";
+import Marquee from "./Marquee";
+import Cards from "./Cards";
+import Footer from "./Footer";
+
+export { AboutPage, LandingPage, Featured, Eyes, Navbar, Marquee, Cards, Footer };
